9761fab0f50c3d908e4c10a366d9ade9e5c3b59a
Heba-Atef99 <hoba30.ha@gmail.com>
2021-07-13 02:51:17 +0200

    added json convert, min, format...all working well :)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,52 +19,126 @@ namespace XML_Editor
 {
     public partial class MainWindow : Window
     {
+        Tree t = new Tree();
+        string sourceDirectory;
+        string inputFileName;
         public MainWindow()
         {
             InitializeComponent();
+            //using scroll bar in input textbox
             input_text.AcceptsReturn = true;
-            input_text.TextWrapping = TextWrapping.Wrap;
-            output_text.AcceptsReturn = true;
-            output_text.TextWrapping = TextWrapping.Wrap;
-            //input_text.Text = "f             input_text.Text = ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff ffffffffffffffffffffffffffffffffddddddddddddddddddddddddddddddddddddddddddddddddddddddddddfinput_text.Text = ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff ffffffffffffffffffffffffffffffffddddddddddddddddddddddddddddddddddddddddddddddddddddddddddf;fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff ffffffffffffffffffffffffffffffffddddddddddddddddddddddddddddddddddddddddddddddddddddddddddf";
-            //input.Document = "f             input_text.Text = ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff ffffffffffffffffffffffffffffffffddddddddddddddddddddddddddddddddddddddddddddddddddddddddddfinput_text.Text = ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff ffffffffffffffffffffffffffffffffddddddddddddddddddddddddddddddddddddddddddddddddddddddddddf;fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff ffffffffffffffffffffffffffffffffddddddddddddddddddddddddddddddddddddddddddddddddddddddddddf";
-
-            //TextRange rangeOfText1 = new TextRange(richTextBox.Document.ContentEnd, richTextBox.Document.ContentEnd);
-            //rangeOfText1.Text = "Text1 ";
-            //rangeOfText1.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Blue);
-            //rangeOfText1.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
-
+            input_text.TextWrapping = TextWrapping.NoWrap;
             ScrollViewer.SetVerticalScrollBarVisibility(input_text, ScrollBarVisibility.Auto);
+            ScrollViewer.SetHorizontalScrollBarVisibility(input_text, ScrollBarVisibility.Auto);
+
+            //using scroll bar in output textbox
+            output_text.AcceptsReturn = true;
+            output_text.TextWrapping = TextWrapping.NoWrap;
             ScrollViewer.SetVerticalScrollBarVisibility(output_text, ScrollBarVisibility.Auto);
+            ScrollViewer.SetHorizontalScrollBarVisibility(output_text, ScrollBarVisibility.Auto);
         }
 
         private void browse_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog input_file = new OpenFileDialog();
 
-            var fileContent = string.Empty;
-            var filePath = string.Empty;
-
-            input_file.InitialDirectory = "c:\\";
-            input_file.Filter = "xml files (*.xml)|*.xml";
+            //input_file.Filter = "txt files (*.txt)|*.txt";
+            //input_file.Filter = "xml files (*.xml)|*.xml";
             input_file.FilterIndex = 2;
             input_file.RestoreDirectory = true;
 
             if (input_file.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
+                output_text.Text = null;
+                
                 //Get the path of specified file
-                filePath = input_file.FileName;
+                var filePath = input_file.FileName;
+                inputFileName = System.IO.Path.GetFileNameWithoutExtension(filePath);
+
+                //get the directory name the file is in
+                sourceDirectory = System.IO.Path.GetDirectoryName(filePath);
 
                 //Read the contents of the file into a stream
                 var fileStream = input_file.OpenFile();
 
                 using (StreamReader reader = new StreamReader(fileStream))
                 {
-                    fileContent = reader.ReadToEnd();
+                    var fileContent = reader.ReadToEnd();
                     input_text.Text = fileContent;
+
+                    //read from the begginig of the file
+                    reader.DiscardBufferedData();
+                    reader.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
+                    
+                    t.insertFile(reader);
+                    reader.Close();
                 }
             }
 
         }
+
+        private void format_Click(object sender, RoutedEventArgs e)
+        {
+            output_text.Text = null;
+
+            string fname = inputFileName + "_Output.xml";
+            //get the new file name you want,the Combine method is strongly recommended
+            string destFileName = System.IO.Path.Combine(sourceDirectory, fname);
+
+            //Operations operation = new Operations(output_text, fname);
+            Operations operation = new Operations(output_text, destFileName, 0);
+            //if (File.Exists(destFileName))
+            //{
+            //    // If file found, delete it    
+            //    File.Delete(destFileName);
+            //}
+
+            operation.Formating(t.getTreeRoot());
+            show_output(destFileName);
+        }
+
+        private void convert_Click(object sender, RoutedEventArgs e)
+        {
+            output_text.Text = null;
+
+            string fname = inputFileName + "_Output.json";
+            //get the new file name you want,the Combine method is strongly recommended
+            string destFileName = System.IO.Path.Combine(sourceDirectory, fname);
+
+            //Operations operation = new Operations(output_text, fname);
+            Operations operation = new Operations(output_text, destFileName, 0);
+            operation.write("{", true);
+            operation.Converting(t.getTreeRoot(), t.getTreeRoot());
+            operation.write("}", true);
+
+            show_output(destFileName);
+        }
+
+        void show_output(string destFileName)
+        {
+            output_text.Text = null;
+
+            using (StreamReader reader = new StreamReader(destFileName))
+            {
+                var fileContent = reader.ReadToEnd();
+                output_text.Text = fileContent;
+                reader.Close();
+            }
+        }
+
+        private void min_Click(object sender, RoutedEventArgs e)
+        {
+            output_text.Text = null;
+
+            string fname = inputFileName + "_min.xml";
+            //get the new file name you want,the Combine method is strongly recommended
+            string destFileName = System.IO.Path.Combine(sourceDirectory, fname);
+
+            //Operations operation = new Operations(output_text, fname);
+            Operations operation = new Operations(output_text, destFileName, 0);
+            operation.Minifying(t.getTreeRoot());
+            show_output(destFileName);
+        }
+
     }
 }
---
+++ Operations.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace XML_Editor
+{
+    class Operations
+    {
+        public int first;
+        TextBox output;
+        public string filename;
+
+        public Operations(TextBox output, string fname, int first)
+        {
+            filename = fname;
+            this.output = output;
+            this.first = first;
+        }
+
+        public void write(string s, bool add)
+        {
+            if (first == 0)
+            {
+                StreamWriter sw = new StreamWriter(filename, false);
+                if (add == true)
+                {
+                    sw.WriteLine(s);
+                    sw.Close();
+                }
+                else
+                {
+                    sw.Write(s);
+                    sw.Close();
+                }
+                first = 1;
+            }
+            else
+            {
+                StreamWriter sw = new StreamWriter(filename, true);
+                if (add == true)
+                {
+                    sw.WriteLine(s);
+                    sw.Close();
+                }
+                else
+                {
+                    sw.Write(s);
+                    sw.Close();
+                }
+            }
+        }
+
+        /*public void Formating(Node root)
+        {
+            bool flag = false;
+            Node r = root;
+            List<Node> children = new List<Node>();
+            children = root.getChildren();
+            if (r == null)
+                return;
+            for (int loop = 0; loop < r.getDepth(); loop++)
+            {
+                write("    ", false);
+            }
+            if (r.getTagAttributes() == null)
+            {
+                write("<" + r.getTagName() + ">", true);
+            }
+            else
+            {
+                write("<" + r.getTagName() + " " + r.getTagAttributes() + ">", true);
+            }
+            if (r.getTagValue() == null)
+            { }
+            else
+            {
+                for (int loop = 0; loop < r.getDepth() + 1; loop++)
+                {
+                    write("    ", false);
+                }
+
+                write(r.getTagValue(), true);
+            }
+            if (r != null)
+            {
+
+                foreach (Node child in children)
+                {
+                    Formating(child);
+                }
+            }
+            if (flag == true)
+            {
+                write("</" + r.getTagName() + ">", true);
+                flag = false;
+            }
+
+            for (int loop = 0; loop < r.getDepth(); loop++)
+            {
+                write("    ", false);
+            }
+
+            write("</" + r.getTagName() + ">", true);
+        }*/
+
+        public void Formating(Node root)
+        {
+            bool flag = false;
+            Node r = root;
+            List<Node> children = new List<Node>();
+            children = root.getChildren();
+            if (r == null)
+                return;
+            for (int loop = 0; loop < r.getDepth(); loop++)
+            {
+                write("    ", false);
+            }
+            if (r.getTagAttributes() == null)
+            {
+                write("<" + r.getTagName() + ">", true);
+            }
+            else
+            {
+                write("<" + r.getTagName() + " " + r.getTagAttributes() + ">", true);
+            }
+            
+            if (r.getTagValue() != null)
+            {
+                for (int loop = 0; loop < r.getDepth() + 1; loop++)
+                {
+                    write("    ", false);
+                }
+
+                write(r.getTagValue(), true);
+            }
+            if (r != null)
+            {
+                foreach (Node child in children)
+                {
+                    Formating(child);
+                }
+            }
+
+            if (r.getIsClosingTag() == false)
+            {
+                for (int loop = 0; loop < r.getDepth(); loop++)
+                {
+                    write("    ", false);
+                }
+                write("</" + r.getTagName() + ">", true);
+            }
+        }
+
+
+
+        //public string MinifyingAUX(Node root)
+        //{
+        //    string xml = "";
+        //    Node r = root;
+        //    List<Node> children = root.getChildren();
+
+        //    if (r == null)
+        //        return "";
+
+        //    if (r.getTagAttributes() == null) { xml += "<" + r.getTagName() + ">"; }
+
+        //    else { xml += "<" + r.getTagName() + " " + r.getTagAttributes() + ">"; }
+
+        //    if (r.getTagValue() == null) { }
+
+        //    else
+        //    {
+        //        string newStr = r.getTagValue().Trim();
+
+        //        xml += newStr;
+        //    }
+
+        //    if (r != null)
+        //    {
+
+        //        foreach (Node child in children)
+        //        {
+        //            xml += MinifyingAUX(child);
+        //        }
+        //    }
+
+        //    xml += "</" + r.getTagName() + ">";
+        //    return xml;
+        //}
+
+        public string MinifyingAUX(Node root)
+        {
+            string xml = "";
+            Node r = root;
+            List<Node> children = root.getChildren();
+            if (r == null)
+                return "";
+            if (r.getTagAttributes() == null) { xml += "<" + r.getTagName() + ">"; }
+            else { xml += "<" + r.getTagName() + " " + r.getTagAttributes() + ">"; }
+
+            if (r.getTagValue() == null) { }
+            else
+            {
+                string newStr = r.getTagValue().Trim();
+                xml += newStr;
+            }
+
+            if (r != null)
+            {
+
+                foreach (Node child in children)
+                {
+                    xml += MinifyingAUX(child);
+                }
+            }
+            if (r.getIsClosingTag())
+            { }
+            else
+            {
+                xml += "</" + r.getTagName() + ">";
+            }
+            return xml;
+        }
+
+        public void Minifying(Node root)
+        {
+            string s = MinifyingAUX(root);
+            File.WriteAllText(filename, s);
+        }
+
+        public void Converting(Node root, Node parent)
+        {
+            Node r = root;
+            int flag = 1;
+            List<Node> children = new List<Node>();
+            children = root.getChildren();
+            List<Node> children_parent = new List<Node>();
+            children_parent = parent.getChildren();
+            if (r != null && r.getChildren().Count != 0 && ((r.getTagName() + "s") != parent.getTagName()))
+            {
+                if (root.getDepth() == 0)
+                {
+                    for (int i = 0; i < r.getDepth() * 4 + 2; i++)
+                    {
+                        write(" ", false);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < r.getDepth() * 4; i++)
+                    {
+                        write(" ", false);
+
+                    }
+                }
+                write('"' + r.getTagName() + '"' + ":" + "{", true);
+            }
+
+            if (r.getChildren().Count == 0
+                && ((r.getTagName() + "s") != parent.getTagName()) && root == children_parent[parent.getChildren().Count - 1])
+            {
+                for (int i = 0; i < r.getDepth() * 4; i++)
+                {
+                    write(" ", false);
+                }
+                write('"' + r.getTagName() + '"' + ":" + '"' + r.getTagValue() + '"', true);
+            }
+
+            if (r.getChildren().Count == 0
+                && ((r.getTagName() + "s") != parent.getTagName()) && root != children_parent[parent.getChildren().Count - 1])
+            {
+                for (int i = 0; i < r.getDepth() * 4; i++)
+                {
+                    write(" ", false);
+                }
+
+                write('"' + r.getTagName() + '"' + ":" + '"' + r.getTagValue() + '"' + ",", true);
+            }
+
+            if (r != null && ((r.getTagName() + "s") == parent.getTagName())
+                && root == children_parent[0] && children_parent.Count > 1)
+            {
+                for (int i = 0; i < r.getDepth() * 4; i++)
+                {
+                    write(" ", false);
+                }
+                write('"' + r.getTagName() + '"' + ":" + "[", true);
+            }
+
+            if (r != null && ((r.getTagName() + "s") == parent.getTagName())
+                && root == children_parent[0] && children_parent.Count == 1)
+            {
+                for (int i = 0; i < r.getDepth() * 4; i++)
+                {
+                    write(" ", false);
+                }
+                write('"' + r.getTagName() + '"' + ":" + "{", true);
+                flag = 0;
+            }
+
+            if (r.getChildren().Count == 0
+                && ((r.getTagName() + "s") == parent.getTagName()) && root == children_parent[parent.getChildren().Count - 1])
+            {
+                for (int i = 0; i < r.getDepth() * 4 + 2; i++)
+                {
+                    write(" ", false);
+                }
+                write('"' + r.getTagValue() + '"', true);
+            }
+            if (r.getChildren().Count == 0
+                && ((r.getTagName() + "s") == parent.getTagName()) && root != children_parent[parent.getChildren().Count - 1])
+            {
+                for (int i = 0; i < r.getDepth() * 4 + 2; i++)
+                {
+                    write(" ", false);
+                }
+                write('"' + r.getTagValue() + '"' + ",", true);
+            }
+            if (r != null && ((r.getTagName() + "s") == parent.getTagName())
+                && children.Count > 0 && flag == 1)
+            {
+                for (int i = 0; i < r.getDepth() * 4 + 2; i++)
+                {
+                    write(" ", false);
+                }
+                write("{", true);
+            }
+            foreach (Node child in children)
+            {
+                Converting(child, root);
+            }
+            if (root.getChildren().Count != 0 && root.getTagName() == (children[0].getTagName() + "s")
+                && children.Count > 1)
+            {
+                for (int i = 0; i < r.getDepth() * 4 + 4; i++)
+                {
+                    write(" ", false);
+                }
+                write("]", true);
+            }
+            if (r != null && r.getChildren().Count != 0
+                && ((root.getTagName() + "s") != parent.getTagName()) && root == children_parent[parent.getChildren().Count - 1] || root.getDepth() == 0)
+            {
+                if (root.getDepth() == 0)
+                {
+                    for (int i = 0; i < r.getDepth() * 4 + 2; i++)
+                    {
+                        write(" ", false);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < r.getDepth() * 4; i++)
+                    {
+                        write(" ", false);
+                    }
+                }
+                write("}", true);
+            }
+            if (r != null && r.getChildren().Count != 0
+                && ((root.getTagName() + "s") != parent.getTagName()) && root != children_parent[parent.getChildren().Count - 1] && root.getDepth() != 0)
+            {
+                for (int i = 0; i < r.getDepth() * 4; i++)
+                {
+                    write(" ", false);
+                }
+                write("},", true);
+            }
+
+            if (r != null && ((r.getTagName() + "s") == parent.getTagName())
+                && children.Count > 0 && root == children_parent[parent.getChildren().Count - 1])
+            {
+                for (int i = 0; i < r.getDepth() * 4 + 2; i++)
+                {
+                    write(" ", false);
+                }
+                write("}", true);
+            }
+            if (r != null && ((r.getTagName() + "s") == parent.getTagName())
+                && children.Count > 0
+                && root != children_parent[parent.getChildren().Count - 1])
+            {
+                for (int i = 0; i < r.getDepth() * 4 + 2; i++)
+                {
+                    write(" ", false);
+                }
+                write("},", true);
+            }
+
+        }
+    }
+}
