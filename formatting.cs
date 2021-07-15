eeccd3009ace36621962b24aabbd449403425271
shimaa88 <73905180+shimaa88@users.noreply.github.com>
2021-07-15 02:25:01 +0200

    Update formatting.cs

--- formatting.cs
+++ formatting.cs
@@ -56,40 +56,44 @@ namespace XML_Editor
             Node r = root;
             List<Node> children = new List<Node>();
             children = root.getChildren();
+            //if node is null will return 
             if (r == null)
                 return;
+            //loop to the depth of each node and write space for indentation
             for (int loop = 0; loop < r.getDepth(); loop++)
             {
                 write("    ", false);
             }
+            //if node not has attribute
             if (r.getTagAttributes() == null)
-            {
+            {//if the tag name is closing tag < id/>
                 if (r.getIsClosingTag())
                     write("<" + r.getTagName() + "/" + ">", true);
-
+                //if the tag name isnot closing tag < id>
                 else
                     write("<" + r.getTagName() + ">", true);
             }
+            //if node has attribute
             else
-            {
+            {//if the tag name is closing tag < id/>
                 if (r.getIsClosingTag())
                     write("<" + r.getTagName() + " " + r.getTagAttributes() + "/" + ">", true);
-
+                  //if the tag name isnot closing tag < id>
                 else
                     write("<" + r.getTagName() + " " + r.getTagAttributes() + ">", true);
             }
-
+              //if node has tag value
             if (r.getTagValue() != null)
-            {
+            {    //loop for the depth of node+1 to make indentation
                 for (int loop = 0; loop < r.getDepth() + 1; loop++)
                 {
                     write("    ", false);
                 }
-
+                //write tag value 
                 write(r.getTagValue(), true);
             }
             if (r != null)
-            {
+            { //loop for each child in the node 
                 foreach (Node child in children)
                 {
                     Format(child);
